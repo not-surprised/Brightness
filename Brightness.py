d0f32c267fcc33d35c4737cf293b96d0332d0f4d
Ivy <67237115+chiang-ivy@users.noreply.github.com>
2021-10-29 16:51:00 -0400

    Update Brightness.py
    Functions to set brightness, get brightness, get monitors

--- Brightness.py
+++ Brightness.py
@@ -1,8 +1,27 @@
 import screen_brightness_control as sbc
 
-current_brightness = sbc.get_brightness()
-print(current_brightness)
-sbc.fade_brightness(75, start=25)
-monitors = sbc.list_monitors()
+def setBrightness(brightness):
+    current_brightness = sbc.get_brightness()
+    sbc.fade_brightness(brightness, start=current_brightness)
+
+def setBrightness2(brightness, monitor):
+    current_brightness =sbc.get_brightness()
+    monitors = sbc.list_monitors()
+    sbc.set_brightness(brightness, display=monitors[monitor])
+
+def getBrightness():
+    return sbc.get_brightness()
+
+def getBrightness2(monitor):
+    return sbc.get_brightness(monitor)
+
+def getMonitors():
+    monitors = sbc.list_monitors()
+    return monitors
+
+# monitors = getMonitors()
 # print(monitors)
-# sbc.set_brightness(75, display=monitors[1])
+# brightness = getBrightness()
+# print(brightness)
+# setBrightness(brightness - 20)
+# # setBrightness2(brightness + 20, 0)
